d7b6109500e7052df5125e84393da3a8d993d66e
ArfhSdt <arfh86sdt@gmail.com>
2026-02-14 20:22:54 +0330

    add root route for index.html

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
+
+// اضافه کردن روت اصلی برای نمایش index.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // اتصال به مونگو اطلس
